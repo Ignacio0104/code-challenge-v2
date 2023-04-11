671dcaa7d48bb045f3b02d410f0a59970dea9349
Nicolas Kaen <nicolas.y.kaen.-nd@disney.com>
2023-04-11 16:20:41 -0700

    fix jest

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
 import Navbar from "./components/Navbar";
-import videoBackground from "./assets/videos/CosmosVideoBackgrounds.mp4";
+// import videoBackground from "./assets/videos/CosmosVideoBackgrounds.mp4";
 import HeroSection from "./components/HeroSection";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CharacterDetails from "./components/CharacterDetails";
@@ -10,16 +10,16 @@ import CharacterDetails from "./components/CharacterDetails";
 const App = () => {
   return (
     <BrowserRouter>
-    <div className="App">
-      <video autoPlay loop muted src={videoBackground}></video>
-      <Navbar></Navbar>
-      <Routes>
-        <Route path='/' element={<HeroSection/>}/>
-        <Route path='/character' element={<CharacterDetails/>}/>
-      </Routes>
-    </div>
+      <div className="App">
+        {/* <video autoPlay loop muted src={videoBackground}></video> */}
+        <Navbar></Navbar>
+        <Routes>
+          <Route path="/" element={<HeroSection />} />
+          <Route path="/character" element={<CharacterDetails />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
